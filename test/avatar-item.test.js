e3727274c4e371f2fdcd8bd817898c267a8281f0
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-28 21:03:37 -0700

    travis fix attempt 1

--- test/avatar-item.test.js
+++ test/avatar-item.test.js
@@ -15,7 +15,7 @@ test('IF I GIVE YOU DATA WILL YOU DISPLAY IT LIKE A GOOD CHILD???', assert => {
 
     assert.htmlEqual(rendered, /*html*/ `
     <li class="avatar">
-        <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
+        <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" onError="this.onerror=null; this.src='data/placeholder.png';" alt="46th Earth King">
         <h2>46th Earth King</h2>
     </li>
     `);
